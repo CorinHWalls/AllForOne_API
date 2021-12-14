31de54bf95e5b70a33cd91a329f0e21fad2dbee8
Corin Walls <cdoubleyou89@gmail.com>
2021-12-13 17:18:48 -0800

    initial commit

--- Program.cs
+++ Program.cs
@@ -8,6 +8,25 @@ builder.Services.AddScoped<GreaterOrLessService>();
 builder.Services.AddScoped<SayHelloService>();
 builder.Services.AddScoped<TwoNumbersService>();
 builder.Services.AddScoped<OddEvenService>();
+builder.Services.AddScoped<ReverseItService>();
+builder.Services.AddScoped<MadLibServices>();
+builder.Services.AddScoped<Magic8BallService>();
+builder.Services.AddScoped<RestaurantPickerService>();
+
+///CORS Policy to access Frontend
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+    builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -24,6 +43,8 @@ if (app.Environment.IsDevelopment())
 }
 
 // app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
